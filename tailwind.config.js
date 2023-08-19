658e615e0791bcd9586048ece1bb010df73c5d2a
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-20 03:04:32 +0600

    added banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "work sans": ["Work Sans", "sans - serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
