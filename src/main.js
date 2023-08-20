b50f22aa6a197ae950666926e082228589d4f639
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-21 01:48:30 +0600

    calculate totalPrice and add product into product list

--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
-function productNameAndPrice(target) {
+function getProductNameAndPrice(target) {
   let productName = target.childNodes[3].childNodes[3].innerText;
   let productPrice = parseFloat(
     target.childNodes[3].childNodes[5].childNodes[1].innerText
-  ).toFixed(2);
-  console.log(productName, productPrice);
+  );
+  putItemsInProductList("productsList", productName);
+  totalPriceCalculation("totalPrice", productPrice);
 }
+
+const putItemsInProductList = (sectionId, productName) => {
+  let itemSection = document.getElementById(sectionId);
+  let item = document.createElement("p");
+  let count = itemSection.childElementCount;
+  item.innerHTML = `${count + 1}. ${productName}`;
+  itemSection.appendChild(item);
+};
+
+const totalPriceCalculation = (totalPriceId, productPrice) => {
+  let totalPrice = parseFloat(document.getElementById(totalPriceId).innerText);
+  totalPrice += productPrice;
+
+  setTotalPrice("totalPrice", totalPrice);
+
+  if (totalPrice >= 1) {
+    buttonAnable("purchaseBtn");
+  }
+  if (totalPrice >= 200) {
+    buttonAnable("applyBtn");
+  }
+};
+
+const setTotalPrice = (totalPriceId, sum) => {
+  document.getElementById(totalPriceId).innerText = sum.toFixed(2);
+};
+
+const buttonAnable = (btnId) => {
+  let btn = document.getElementById(btnId);
+  btn.removeAttribute("disabled");
+  btn.classList.remove("opacity-30");
+};
+
